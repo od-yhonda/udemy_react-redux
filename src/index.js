455a262ef4efa0b2b54f14bb4916d9102b881e73
yhonda <yhonda@opendoor.co.jp>
2019-06-12 17:23:21 +0900

    section1-lesson15

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello React</div>, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
-var publicDir = __dirname + '/public';
+const path = require('path');
+// const ExtractTextPlugin = require('path');
 
-module.exports = {
+const publicDir = path.join(__dirname, '/public');
+
+module.exports = [{
   entry: [
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
     path: publicDir,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    // loader -> rules
+    // webpack8系からloader -> rules
     rules: [{
+      // 最新をインストールするためにbabel-core -> @babel/core
+      // babel-loader8系をならcore7系以上じゃないといけない
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['@babel/preset-react', '@babel/preset-env']
-      }
-    }]
+        // 最新バージョンをインストールするために変更
+        // envはes2015、2016、2017など全部入り。@babel/preset-es2015でインストールでも可。
+        presets: ['@babel/preset-react', '@babel/preset-env'],
+      },
+    }],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: publicDir
-  }
-};
+    contentBase: publicDir,
+  },
+},
+// {
+//   entry: {
+//     style: './stylesheets/index.scss',
+//   },
+//   output: {
+//     path: publicDir,
+//     publicPath: '/',
+//     filename: 'bundle.css',
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.css$/,
+//         loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+//       },
+//       {
+//         test: /\.scss$/,
+//         loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new ExtractTextPlugin('bundle.css'),
+//   ],
+// },
+];
